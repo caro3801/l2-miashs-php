648ec25d162c74ff852953d2127418a4c7cabfd6
Caroline Desprat <caroline.desprat@gmail.com>
2018-02-08 12:24:14 +0100

    corrige tp5 + anon

--- TP/TP4/correction/exercice3/connect.php
+++ TP/TP4/correction/exercice3/connect.php
@@ -1,7 +1,7 @@
 ﻿<?php
 // fichier contenant les constantes de connexion
-define('NOM',"caroline"); // votre login sur mass-etudiant
-define('PASSE',"caroline"); // votre mot de passe sur mass-cara2
-define('SERVEUR',"localhost"); // IP de mass-cara2 172.0.0.1
-define('BASE', "l2web"); // nom de la base de données ex: 17_L2_...
+define('NOM',"!!!"); // votre login sur mass-etudiant
+define('PASSE',"!!!"); // votre mot de passe sur mass-cara2
+define('SERVEUR',"!!!"); // IP de mass-cara2 172.0.0.1
+define('BASE', "!!!"); // nom de la base de données ex: 17_L2_...
 ?>
