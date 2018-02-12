80f6534f530ba9ad8fbacc254ec049593e0ce673
Invité <invite@pioupiou.local>
2018-02-12 15:25:08 +0100

    seance 2

--- TP/TP5/correction/connect.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-// fichier contenant les constantes de connexion
-define('NOM',"!!!"); // votre login sur mass-etudiant
-define('PASSE',"!!!"); // votre mot de passe sur mass-cara2
-define('SERVEUR',"!!!"); // IP de mass-cara2 172.0.0.1
-define('BASE', "!!!"); // nom de la base de données ex: 17_L2_...
-?>
